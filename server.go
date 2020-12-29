61920750b88baf215e69f6e723648ec0114c3854
tomfinney <tomfinney25@gmail.com>
2020-12-29 21:04:58 +0000

    adds dumb pg connector

--- server.go
+++ server.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
+
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
+
+	connectToDb()
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
